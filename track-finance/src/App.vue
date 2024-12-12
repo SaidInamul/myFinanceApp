207f92eb18b6399fbb541721e9f7c98947cc5e0a
SaidHafizzan <100667330+SaidInamul@users.noreply.github.com>
2024-12-12 14:16:12 +0800

    Setting up the components needed
    Components:
    add-transaction.vue
    balance.vue
    header.vue
    income-expenses.vue
    transaction-list.vue

---
+++ track-finance/src/App.vue
@@ -0,0 +1,25 @@
+<script>
+  import Header from './components/header.vue'
+  import Balance from './components/balance.vue'
+  import incomeExpenses from './components/income-expenses.vue'
+  import transactionList from './components/transaction-list.vue'
+  import addTransaction from './components/add-transaction.vue'
+
+  export default {
+    components : {
+      Header, Balance, incomeExpenses, transactionList, addTransaction
+    }
+  }
+
+</script>
+
+<template>
+    <Header />
+    <div class="container">
+      <Balance />
+      <incomeExpenses />
+      <transactionList />
+      <addTransaction />
+    </div>
+    
+</template>
---
+++ track-finance/src/components/header.vue
@@ -0,0 +1,3 @@
+<template>
+    <h2>Finance Tracker</h2>
+</template>
