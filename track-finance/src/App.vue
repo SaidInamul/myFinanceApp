bc50ef11ba29eb97f3d6f9ce5e67ac31c5031a9e
SaidHafizzan <100667330+SaidInamul@users.noreply.github.com>
2024-12-12 21:14:44 +0800

    calculating the total balance
    Using the reduce method which is a built-in method of an array in javascript. Usually used to sum up, flattening array and creating object from an array.

--- track-finance/src/App.vue
+++ track-finance/src/App.vue
@@ -4,20 +4,26 @@
   import incomeExpenses from './components/income-expenses.vue'
   import transactionList from './components/transaction-list.vue'
   import addTransaction from './components/add-transaction.vue'
-  import { ref } from 'vue'
+  import { ref, computed } from 'vue'
 
   const transactions = ref([
     {id : 1, name : 'Rental', price : -800},
     {id : 2, name : 'Food', price : -1000},
-    {id : 3, name : 'PayCheck', price : 2000},
+    {id : 3, name : 'PayCheck', price : 3000},
     {id : 4, name : 'Telco', price : -50},
   ])
+
+  const total = computed( () => {
+    return transactions.value.reduce((acc, transaction) => {
+      return acc + transaction.price
+    }, 0)
+  })
 </script>
 
 <template>
     <Header />
     <div class="container">
-      <Balance />
+      <Balance :total="total"/>
       <incomeExpenses />
       <transactionList :transactions="transactions"/>
       <addTransaction />
